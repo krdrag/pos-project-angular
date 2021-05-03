b3b4f029539e4fd759a72935f18160db83823a88
krdrag <krzysztof.dra@gmail.com>
2021-05-03 22:00:19 +0200

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { WorkstationState } from './stores/workstation/workstation.state';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxsModule } from '@ngxs/store';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { PosComponent } from './components/pos/pos.component';
+import { FormsModule } from '@angular/forms';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PosComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path: 'login', component: LoginComponent},
+      {path: 'pos', component: PosComponent},
+      { path: '', redirectTo: '/pos', pathMatch: 'full' }
+    ]),
     NgxsModule.forRoot([
       WorkstationState,
       TransactionState
