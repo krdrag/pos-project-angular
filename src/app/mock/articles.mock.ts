1272496aeb13de01a308ad7c695d285d8fdb9b4b
krdrag <krzysztof.dra@gmail.com>
2021-05-03 10:59:35 +0200

    Create articles.mock.ts

---
+++ src/app/mock/articles.mock.ts
@@ -0,0 +1,9 @@
+import { Article } from './../models/article.model';
+
+
+export const Articles: Article[] = [
+    {itemID: "1234567"},
+    {itemID: "1111111"},
+    {itemID: "2222222"},
+    {itemID: "3333333"},
+];
