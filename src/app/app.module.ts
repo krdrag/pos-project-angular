561ba7cc9e83f31d1a449e4056e6fc39955c0ccf
krdrag <krzysztof.dra@gmail.com>
2021-05-06 15:06:26 +0200

    add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LoginComponent } from './components/login/login.component';
 import { PosComponent } from './components/pos/pos.component';
 import { FormsModule } from '@angular/forms';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -36,6 +37,7 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
       TransactionState,
       UserState
     ], { developmentMode: true }),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
