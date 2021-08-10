eb1747ad15162e976919716eb3bb97ff2d9c647b
krdrag <krzysztof.dra@gmail.com>
2021-08-10 20:52:13 +0200

    Show toast after clicking icons

--- src/app/components/general/navbar/navbar.component.ts
+++ src/app/components/general/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { faDatabase } from '@fortawesome/free-solid-svg-icons';
 import { faHdd } from '@fortawesome/free-solid-svg-icons';
 import { faGift } from '@fortawesome/free-solid-svg-icons';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-navbar',
@@ -14,9 +15,28 @@ export class NavbarComponent implements OnInit {
   faHdd = faHdd;
   faGift = faGift;
 
-  constructor() { }
+  constructor(private toastr: ToastrService) { }
 
   ngOnInit(): void {
+  }
+
+  LoyaltyStatus() {
+    this.showToast("Service status", "Loyalty system is online!");
+  }
+
+  DevicesStatus() {
+    this.showToast("Service status", "Devices are connected!");
+  }
+
+  StoreServerStatus() {
+    this.showToast("Service status", "Store server is reachable!");
+  }
+
+  showToast(title: string, message: string) {
+    this.toastr.success(message, title, {
+      positionClass: 'toast-top-center',
+      timeOut: 2000
+    });
   }
 
 }
