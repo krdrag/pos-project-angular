3a9b99054e0ca7db539ce2dfd7a00beaeb3a0bf8
krdrag <krzysztof.dra@gmail.com>
2021-08-13 22:04:12 +0200

    Sidebar finished

--- src/app/components/general/sidebar-footer/sidebar-footer.component.ts
+++ src/app/components/general/sidebar-footer/sidebar-footer.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { faCircle } from '@fortawesome/free-solid-svg-icons';
+import { UserService } from 'src/app/services/general/user.service';
+import { WorkstationService } from 'src/app/services/general/workstation.service';
+import { User } from '../../../models/user.model';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-sidebar-footer',
@@ -9,10 +13,21 @@ import { faCircle } from '@fortawesome/free-solid-svg-icons';
 export class SidebarFooterComponent implements OnInit {
 
   faCircle = faCircle;
+  user: User;
+  workstation;
 
-  constructor() { }
+  constructor(
+    private translate: TranslateService, 
+    private userService: UserService,
+    private WorkstationService: WorkstationService) { }
 
   ngOnInit(): void {
+    this.user = this.userService.GetUser();
+    this.workstation = this.WorkstationService.getWorkstationData();
+  }
+
+  useLanguage(language: string): void {
+    this.translate.use(language);
   }
 
 }
--- src/app/mock/users.mock.ts
+++ src/app/mock/users.mock.ts
@@ -1,4 +1,4 @@
 export const Users = [
-    {cashierID: "1", password: "1", role: "Administrator"},
-    {cashierID: "100", password: "100", role: "Cashier"}
+    {cashierID: "1", password: "1", role: "Administrator", name: "John", lastName: "Doe"},
+    {cashierID: "100", password: "100", role: "Cashier", name: "Tom", lastName: "Stonks"}
 ];
